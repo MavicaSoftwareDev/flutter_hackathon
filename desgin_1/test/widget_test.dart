2567e5f365b45bb73925e38b2ce5372d29d62d6c
Mohamad amgad elsaid <65943047+MavicaSoftwareDev@users.noreply.github.com>
2022-10-12 17:20:47 +0200

    desgin 1 ready.

--- desgin_1/test/widget_test.dart
+++ desgin_1/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:desgin_1/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const Design1());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
